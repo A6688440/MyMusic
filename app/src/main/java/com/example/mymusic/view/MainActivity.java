ef91f5553474e705b42b3d116e20a1ab2150aff8
SJC <1591417120@qq.com>
2020-04-20 01:50:57 +0800

    完成主界面的UI

--- app/src/main/java/com/example/mymusic/view/MainActivity.java
+++ app/src/main/java/com/example/mymusic/view/MainActivity.java
@@ -2,21 +2,40 @@ package com.example.mymusic.view;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import com.andexert.library.RippleView;
 import com.example.mymusic.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.play_seek_bar)
+    SeekBar playSeekBar;
+    @BindView(R.id.song_name)
+    TextView songName;
+    @BindView(R.id.btn_player)
+    Button btnPlayer;
+    @BindView(R.id.song_next)
+    RippleView songNext;
+    @BindView(R.id.circle_singer_image)
+    CircleImageView circleSingerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container,new MainFragment())
+                .commit();
 
     }
 }
